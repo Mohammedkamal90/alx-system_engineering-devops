31e839e031a7a0ad8b6214f0254990f7b58ae6ff
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-01-02 11:21:21 -0800

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,60 +1,31 @@
 #!/usr/bin/python3
 """
-Using https://jsonplaceholder.typicode.com
-returns info about employee TODO progress
-Implemented using recursion
+Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
 import re
 import requests
 import sys
 
+
 API = "https://jsonplaceholder.typicode.com"
-"""REST API url"""
-
-
-def get_user_data(employee_id):
-    user_res = requests.get('{}/users/{}'.format(API, employee_id))
-    user_data = user_res.json()
-
-    if user_res.status_code != 200:
-        print("Error: Unable to fetch user data")
-        sys.exit(1)
-
-    return user_data
-
-
-def get_todo_data():
-    todos_res = requests.get('{}/todos'.format(API))
-    todos_data = todos_res.json()
-
-    if todos_res.status_code != 200:
-        print("Error: Unable to fetch TODO list data")
-        sys.exit(1)
-
-    return todos_data
-
-
-def print_todo_progress(user_name, todos_done, total_todos):
-    print(
-        'Employee {} is done with tasks({}/{}):'.format(
-            user_name, len(todos_done), total_todos
-        )
-    )
-    for todo_done in todos_done:
-        print('\t {}'.format(todo_done.get('title')))
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
-            employee_id = int(sys.argv[1])
-
-            user_data = get_user_data(employee_id)
-            user_name = user_data.get('name')
-
-            todos_data = get_todo_data()
-            todos = list\n(filter(lambda x: x.get('userId')== employee_id, todos_data))
-
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API, id)).json()
+            todos_res = requests.get('{}/todos'.format(API)).json()
+            user_name = user_res.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
             todos_done = list(filter(lambda x: x.get('completed'), todos))
-
-            print_todo_progress(user_name, todos_done, len(todos))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
