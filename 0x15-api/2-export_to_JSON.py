bd0d05d7d007e023d075bf765f9c9425c95a1301
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-01-02 10:35:34 -0800

    task2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -7,56 +7,23 @@ import requests
 import sys
 
 
-def get_user_data(employee_id):
-    url_for_users = 'https://jsonplaceholder.typicode.com/users/{}'
-            .format(employee_id)
-    response = requests.get(url_for_users)
-
-    if response.status_code != 200:
-        print("Error: Unable to fetch user data")
-        sys.exit(1)
-
-    return response.json()
-
-
-def get_todo_data():
-    url_for_todos = 'https://jsonplaceholder.typicode.com/todos'
-    response = requests.get(url_for_todos)
-
-    if response.status_code != 200:
-        print("Error: Unable to fetch TODO list data")
-        sys.exit(1)
-
-    return response.json()
-
-
-def export_to_json(user_id, username, todo_data):
-    filename = '{}.json'.format(user_id)
-
-    json_data = {str(user_id): []}
-
-    for todo in todo_data:
-        task_title = todo.get('title')
-        task_completed_status = todo.get('completed')
-        json_data[str(user_id)].append({
-            "task": task_title,
-            "completed": task_completed_status,
-            "username": username})
-
-    with open(filename, 'w+') as json_file:
-        json.dump(json_data, json_file)
-
-    print("Data exported to {}".format(filename))
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
 
-    employee_id = int(sys.argv[1])
+    DONE_TASKS = 0
+    ALL_TASKS = 0
+    csvList = []
 
-    user_data = get_user_data(employee_id)
-    todo_data = get_todo_data()
+    URL_FOR_USERS = 'https://jsonplaceholder.typicode.com/users/{0}'.\
+        format(sys.argv[1])
+    URL_FOR_TODOS = 'https://jsonplaceholder.typicode.com/todos'
+    r_for_users = requests.get(URL_FOR_USERS)
+    r_for_todos = requests.get(URL_FOR_TODOS)
 
-    export_to_json(employee_id, user_data.get('username'), todo_data)
+    name = r_for_users.json().get('name')
+    user_name = r_for_users.json().get('username')
+    todos = r_for_todos.json()
+    with open(sys.argv[1] + '.json', 'w+') as f:
+        json.dump({sys.argv[1]: [{
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": user_name} for todo in todos]}, f)
