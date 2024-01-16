368b28f135077833cdbd4493580e15971d9d9f56
Mohammedkamal90 <mohammed.kamal14590@gmail.com>
2024-01-16 02:18:00 -0800

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,38 +1,16 @@
 #!/usr/bin/python3
-
+"""function to query Subscriber on given Reddit subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """return total number of subscribers on given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """return number of subscribers on given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-
-        if response.status_code == 404:
-            return 0
-
-        results = response.json().get("data")
-        subscribers = results.get("subscribers", 0)
-        return subscribers
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        subscribers = number_of_subscribers(subreddit)
-        print(subscribers)
-
+    results = response.json().get("data")
+    return results.get("subscribers")
